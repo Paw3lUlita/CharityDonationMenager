75d9d25262fb0978ceb41f6a23ce1ec05a96d1bd
Paw3lUlita <pawel.ulita33@gmail.com>
2022-09-08 19:43:08 +0200

    all categories method in donation service

--- src/main/java/pl/coderslab/charity/controller/DonationController.java
+++ src/main/java/pl/coderslab/charity/controller/DonationController.java
@@ -2,11 +2,25 @@ package pl.coderslab.charity.controller;
 
 import lombok.AllArgsConstructor;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.RequestMapping;
+import pl.coderslab.charity.entity.Category;
+import pl.coderslab.charity.service.CategoryService;
+import pl.coderslab.charity.service.DonationService;
+
+import java.util.List;
 
 @Controller
 @AllArgsConstructor
+@RequestMapping("/donation")
 public class DonationController {
 
+    private final CategoryService categoryService;
+    private final DonationService donationService;
 
+    @ModelAttribute("allCategories")
+    public List<Category> getAllCategory(){
+        return categoryService.findAll();
+    }
 
 }
