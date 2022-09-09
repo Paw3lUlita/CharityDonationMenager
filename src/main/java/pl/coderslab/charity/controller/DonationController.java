40c8892ded2f84fe18634a752a61486481542690
Paw3lUlita <pawel.ulita33@gmail.com>
2022-09-09 14:39:07 +0200

    excluding footer and header from form, show method in donation controller

--- src/main/java/pl/coderslab/charity/controller/DonationController.java
+++ src/main/java/pl/coderslab/charity/controller/DonationController.java
@@ -2,9 +2,12 @@ package pl.coderslab.charity.controller;
 
 import lombok.AllArgsConstructor;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
 import pl.coderslab.charity.entity.Category;
+import pl.coderslab.charity.entity.Donation;
 import pl.coderslab.charity.service.CategoryService;
 import pl.coderslab.charity.service.DonationService;
 
@@ -17,6 +20,13 @@ public class DonationController {
 
     private final CategoryService categoryService;
     private final DonationService donationService;
+
+    @GetMapping("/donate")
+    public String showDonationForm(Model model){
+        Donation donation = new Donation();
+        model.addAttribute("donation", donation);
+        return "form/form";
+    }
 
     @ModelAttribute("allCategories")
     public List<Category> getAllCategory(){
